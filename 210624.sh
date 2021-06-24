831f19fdab3d147ea393b0e1a7755a82ce1f859b
Barleysack <hookypooky@naver.com>
2021-06-24 11:37:14 +0900

    date

---
+++ 210624.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+touch test1.material
+cp test1.material test1_clone.material
+rm test1_clone.material
+cat test1_clone.material
+mv test1.material test2.txt
+clear
+sudo chmod 777 test2.txt
+echo "little procedure ongoing">>test2.txt
+
+
+cat test2.txt
+
+
