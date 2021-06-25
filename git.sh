fa28600a6dca46142878e3b58018d76f5f0b8645
Barleysack <hookypooky@naver.com>
2021-06-25 14:16:22 +0900

    2021. 06. 25. (금) 14:16:22 KST

--- git.sh
+++ git.sh
@@ -1,14 +1,40 @@
 #!/bin/bash
 Today=$(date)
+echo "스마트 팩토리 여러분들을 위한 깃 쉘"
+if [ -e '/usr/bin/git' ]
+	then
+		echo "설치된 깃을 발견했습니다."
+	else
+		echo "깃을 설치합니다..."
+		sudo apt-get install git -y
+		git config --global user.name [깃 아이디를 대괄호 지우고 입력]
+		git config --global user.email [깃 이메일을 대괄호 지우고 입력]
+		git config credential.helper store
+		
 
+fi
 
 if [ -e './.git' ] 
 	then
 		git add .
 		git commit -m "$Today"
+		echo ==========================================
+		echo ==========================================
+		echo ==========================================
+		echo ==========================================
+		echo ==========================================
+		echo ==========================================
+		echo ==========================================
+		echo ==========================================
+		echo ==========================================
 		git push
+		git config credential.helper store
+
 	else
-		git clone [본인저장소  주소를 대괄호 없이 입력] 
+		git clone [본인저장소  주소를 대괄호 없이 입력]
+		echo "저장소를 받아왔습니다. 이제부터  이 곳에 파일을 저장하시면 됩니다."
+		echo "파일을 넣고 다시 한번 이 쉘을 실행해 주세요."
+		
 fi
 
 
