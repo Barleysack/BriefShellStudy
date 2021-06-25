875008aa1592dfc243a016a9c3f0810bdca2fed5
Barleysack <hookypooky@naver.com>
2021-06-25 14:51:48 +0900

    2021. 06. 25. (금) 14:51:48 KST

--- shell_var.sh
+++ shell_var.sh
@@ -8,3 +8,27 @@ echo '$SMART3='${SMART3:=300}
 SMART4=
 echo '$SMART4='$SMART4
 echo '$SMART4='${SMART4:=400}
+SMART5=
+echo '$SMART5='${SMART5:+500}
+SMART5=100
+echo '$SMART5='${SMART5:+500}
+SMART6=600
+echo '$SMART6='${SMART5:?600}
+SMART6=100
+echo '$SMART6='${SMART6:?" IS EMPTY"}
+echo ==================
+echo ==================
+echo =================
+DATA1="WE ARE SMOLT"
+echo "1:$DATA1"
+echo "2:${DATA1:-"HIHI!"}"
+echo "3:${DATA2:-"HIHI2"}"
+echo "4:${DATA1:="HIHI3"}"
+echo "5:${DATA2:="HIHI4"}"
+echo "6:${DATA2:="HIHI5"}"
+echo "7:${DATA1:+"HIHI6"}"
+echo "8:${DATA1:?"HIHI7"}"
+echo "9:${DATA3:?"HIHI8"}"
+
+echo =======================
+
---
+++ string_test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+PATH_="/home/bnk/code/bnk1/code1/bnk2/code2"
+echo ${PATH_%/bnk*}
+echo ${PATH_%%/bnk*}
